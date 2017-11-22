b057be8d9fe494cf88fbc80bb7d60a5a53326e49
Jose Deuteros <josewbmx@gmail.com>
2017-11-21 21:12:55 -0600

    Se agregó el código para simular el contador funcionando

--- src/app/posts/inicio/inicio.component.ts
+++ src/app/posts/inicio/inicio.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Post } from '../post';
 import { PostsService } from '../posts.service';
 import { Router } from '@angular/router';
 import { trigger, state, style, animate,transition } from '@angular/animations';
 import { DomSanitizer } from '@angular/platform-browser';
-
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-inicio',
@@ -26,8 +26,16 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class InicioComponent implements OnInit {
 
-  show = false;
-  
+
+    private future: Date;
+    private futureString: string;
+    private diff: number;
+    private $counter: Observable<number>;
+    private subscription: Subscription;
+    private message: string;
+
+
+  show = false;  
   posts_Home: Post[];
   posts: Post[];
   
@@ -40,14 +48,31 @@ export class InicioComponent implements OnInit {
   loadMoreVisible : boolean;
   selectedClass: number;
 
-  constructor(private postsService: PostsService, private router: Router,  private sanitizer: DomSanitizer) { 
+  constructor(private postsService: PostsService, private router: Router,  private sanitizer: DomSanitizer, elm: ElementRef) { 
     this.arregloCountCat = [];
-    this.loadMoreVisible = false
-
+    this.loadMoreVisible = false;
+    this.futureString = "July 1, 2018 00:00:00";
+ 
    
   }
 
 
+dhms(t) {
+        var days, hours, minutes, seconds;
+        days = Math.floor(t / 86400);
+        t -= days * 86400;
+        hours = Math.floor(t / 3600) % 24;
+        t -= hours * 3600;
+        minutes = Math.floor(t / 60) % 60;
+        t -= minutes * 60;
+        seconds = t % 60;
+
+        return [
+            days + 'd',
+            hours + 'h',
+            minutes + 'm'
+        ].join(' ');
+    }
 
 
 
@@ -87,7 +112,8 @@ export class InicioComponent implements OnInit {
   }
 
    ngOnDestroy() {
-     console.log("Destroy*******")
+     console.log("Destroy*******");
+     this.subscription.unsubscribe();
 
   }
 
@@ -96,7 +122,14 @@ export class InicioComponent implements OnInit {
 
     this.getPosts_Home();
 
-  	 
+
+     this.future = new Date(this.futureString);
+        this.$counter = Observable.interval(1000).map((x) => {
+            this.diff = Math.floor((this.future.getTime() - new Date().getTime()) / 1000);
+            return x;
+        });
+
+        this.subscription = this.$counter.subscribe((x) => this.message = this.dhms(this.diff));
   }
 
 
