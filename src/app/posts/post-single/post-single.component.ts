35574327c93b6194e37f99b23a64b173b9c6b8df
Jose Deuteros <josewbmx@gmail.com>
2017-11-25 15:45:57 -0600

    Se agregó el menu general en el componente de Post

--- src/app/posts/post-single/post-single.component.ts
+++ src/app/posts/post-single/post-single.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Post } from '../post';
 import { PostsService } from '../posts.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 
 
@@ -14,11 +16,70 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class PostSingleComponent implements OnInit {
 
-	post: Post;
+	 post: Post;
+    private future: Date;
+    private futureString: string;
+    private diff: number;
+    private $counter: Observable<number>;
+    private $counter2: Observable<number>;
+    private subscription: Subscription;
+    private subscription2: Subscription;
+    private dias: string;
+    private horas_minutos: string;
+
+
+  show = false;  
+  posts_Home: Post[];
+  posts: Post[];
+  
+
+  categorias: Post[];
+  post_count:number;
+  categoriasCount:any;
+  arregloCountCat:any;
+  imageX:any;
+  loadMoreVisible : boolean;
+  selectedClass: number;
  
- constructor(private postsService: PostsService, private route: ActivatedRoute) { 
+ constructor(private postsService: PostsService,  private router: Router,  private route: ActivatedRoute, private sanitizer: DomSanitizer, elm: ElementRef) { 
+   this.arregloCountCat = [];
+    this.loadMoreVisible = false;
+    this.futureString = "July 1, 2018 00:00:00";
+    this.post_count = 20;
       
   }
+
+
+  dhms(t) {
+        var days, hours, minutes, seconds;
+        days = Math.floor(t / 86400);
+        t -= days * 86400;
+         
+
+        return [
+            days,
+        ].join(' ');
+    }
+
+
+dhms2(t) {
+         var days, hours, minutes, seconds;
+        days = Math.floor(t / 86400);
+        t -= days * 86400;
+        hours = Math.floor(t / 3600) % 24;
+        t -= hours * 3600;
+        minutes = Math.floor(t / 60) % 60;
+        t -= minutes * 60;
+        seconds = t % 60;
+
+
+        return [ 
+            hours + ' h',
+            minutes + ' m'
+
+        ].join(' ');
+    }
+
 
 
   getPost(slug){
@@ -39,6 +100,26 @@ export class PostSingleComponent implements OnInit {
        this.getPost(slug)
     });
 
+
+        this.future = new Date(this.futureString); 
+        this.$counter = Observable.interval(1000).map((x) => {
+            this.diff = Math.floor((this.future.getTime() - new Date().getTime()) / 1000);
+            return x;
+        });
+
+        this.$counter2 = Observable.interval(1000).map((x) => {
+            this.diff = Math.floor((this.future.getTime() - new Date().getTime()) / 1000);
+            return x;
+        });
+
+        this.subscription = this.$counter.subscribe((x) => this.dias = this.dhms(this.diff));
+        this.subscription2 = this.$counter2.subscribe((x) => this.horas_minutos = this.dhms2(this.diff));
+
+  }
+
+  GoToInicio() {
+     this.router.navigate([""]);
+    console.log("Go to Inicio")
   }
 
 }
